20a9c69835846bb7bb0a43c71cdfe519d9d7bab8
Iury Lamonie <iury.lamonie@hotmail.com>
2020-11-25 14:42:37 -0300

    Fixing Tabuleiro class

--- src/br/imd/Models/Tabuleiro.java
+++ src/br/imd/Models/Tabuleiro.java
@@ -2,12 +2,7 @@ package br.imd.Models;
 
 import java.util.Vector;
 
-import br.imd.Models.Monstro;
-import br.imd.Models.Carta;
-import br.imd.Models.Campo;
-import br.imd.Models.Jogador;
-import br.imd.Models.Magia;
-
+import br.imd.Constants.PosicaoMonstro;
 /**
  * Representa o tabuleiro de um jogador dentro de um duelo.
  * @author Iury
@@ -54,25 +49,77 @@ public class Tabuleiro {
 		Carta cartaEfeito = this.campo.getEspacoMonstro(posicaoCartaAtivou );
 		if( cartaEfeito instanceof MonstroEfeito ) {
 			((MonstroEfeito) cartaEfeito).ativarEfeito(jogadorAtivou, posicaoCartaAtivou, jogadorAlvo, posicaoCartaAlvo);
+			
 		}
 	}
 	
-	public void ativarEfeitoMagia(Magia magia) {
+	/**
+	 * Ativa o efeito de uma marca magica no campo.
+	 * @param jogadorAtivou jogador que ativou o efeito.
+	 * @param posicaoCartaAtivou posição da carta magica na zona de magias.
+	 */
+	public void ativarEfeitoMagia(Jogador jogadorAtivou, int posicaoCartaAtivou, Jogador  jogadorAlvo, int posicaoCartaAlvo) {
+		//Carta cartaMagia = this.campo.getEspacoMagia( posicaoCartaAtivou );
+		// TODO precisa das cartas Magias Feitas
+	}
+	
+	/**
+	 * Muda a posição de um monstro localizado em uma determinada posição
+	 * na zona de monstros.
+	 * @param posicaoMonstroCamp
+	 */
+	public void mudarPosicao(int posicaoMonstroCamp ) {
+		
+		PosicaoMonstro pm = this.campo.getEspacoMonstro(posicaoMonstroCamp).getPosicaoMonstro();
+		switch(pm) {
+		case ATAQUE:
+			this.campo.getEspacoMonstro(posicaoMonstroCamp).setPosicaoMonstro(PosicaoMonstro.DEFESA);
+			break;
+		case DEFESA:
+			this.campo.getEspacoMonstro(posicaoMonstroCamp).setPosicaoMonstro(PosicaoMonstro.ATAQUE);
+			break;
+		case NAO_INVOCADO:
+			//TODO o que fazer quando o monstro não teve seu campo alterado?
+			break;
+		}
 		
 	}
 	
-	public void mudarPosicao(Monstro monstro) {
+	/**
+	 * Envia uma carta que está localizada no campo para o cemiterio.
+	 * @param posicaoCartaCamp
+	 */
+	public void enviarCemiterio( Carta carta, int posicaoCamp ) {
 		
+		this.cemiterio.add(carta);
+		
+		if( carta instanceof Monstro ) {
+			this.campo.setEspacoMonstro(null, posicaoCamp);
+		}
+		else if( carta instanceof Magia ) {
+			this.campo.setEspacoMagia(null, posicaoCamp);
+		}
 	}
 	
-	public void enviarCemiterio(Carta carta) {
-		
-	}
-	
+	/**
+	 * Coloca no tabuleiro um mostro localizado em um determinada posição da mão.
+	 * @param monstro a posição determinada.
+	 */
 	public void colocarMostroTabuleiro(int monstro) {
-		
+		Carta cm = this.maoJogador.remove(monstro);
+		int espacoVazio = this.campo.espacoZonaMonstroVazio();
+		this.campo.setEspacoMonstro((Monstro) cm, espacoVazio);	
 	}
 	
+	/**
+	 * Coloca no tabuleiro uma magia localizado em um determinada posição da mão.
+	 * @param monstro a posição determinada.
+	 */
+	public void colocarMagiaTabuleiro(int magia) {
+		Carta cm = this.maoJogador.remove(magia);
+		int espacoVazio = this.campo.espacoZonaMagiaVazio();
+		this.campo.setEspacoMagia((Magia) cm, espacoVazio);
+	}
 	
 	public Vector<Carta> getCemiterio() {
 		return this.cemiterio;
