4762c0c2d39ab240bf0031b01db9d5f6a96bbe47
Iury Lamonie <iury.lamonie@hotmail.com>
2020-11-25 14:57:01 -0300

    Modifyng Jogador class

--- src/br/imd/Models/Jogador.java
+++ src/br/imd/Models/Jogador.java
@@ -1,6 +1,12 @@
 package br.imd.Models;
 
+/**
+ * Classe que representa o jogador.
+ * @author Iury
+ *
+ */
 public class Jogador {
+	
 	private int pontosVida;
 	private String nome;
 	private Tabuleiro tabuleiro;
@@ -8,7 +14,41 @@ public class Jogador {
 	Jogador(String nome, Baralho baralho){
 		this.pontosVida = 0;
 		this.nome = nome;
-		setTabuleiro(new Tabuleiro(baralho));
+		this.tabuleiro = new Tabuleiro(baralho);
+	}
+	
+	/**
+	 * Coloca um carta monstro localizada na mão do jogador
+	 * em um espaço vazio na zona de monstros.
+	 * @param monstro a posição da carta monstro na mão do jogador.
+	 */
+	public void invocarMonstro(int monstro) {
+		this.tabuleiro.colocarMostroTabuleiro(monstro);
+	}
+	
+	/**
+	 * Coloca um carta monstro localizada na mão do jogador
+	 * em um espaço vazio na zona de monstros.
+	 * @param magia a posição da carta magica na mão do jogador.
+	 */
+	public void invocarMagia(int magia) {
+		this.tabuleiro.colocarMagiaTabuleiro(magia);
+	}
+	
+	
+	public void mudarPosicaoBatalha(Monstro carta) {
+		this.tabuleiro.mudarPosicao(carta.getLocalizacaoTabuleiro());
+	}
+
+	public void comprarCarta() {
+		this.tabuleiro.comprar();
+	}
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	public void setTabuleiro(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
 	}
 	
 	public int getPontosVida() {
@@ -25,25 +65,5 @@ public class Jogador {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public void invocarMonstro(int monstro) {
-		
-	}
-	
-	public void invocarMagia(MagiaNormal magia) {
-			
-	}
-	
-	public void mudarPosicaoBatalha(Monstro carta) {
-		
-	}
-
-	public Tabuleiro getTabuleiro() {
-		return tabuleiro;
-	}
-
-	public void setTabuleiro(Tabuleiro tabuleiro) {
-		this.tabuleiro = tabuleiro;
 	}
 }
